c347bc5c06a5c795ed30f21ff8e93c8e2a5b1996
fructuosowipf <beide@fructuosowipf.com>
2020-06-16 15:24:49 +0200

    enter components

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <vue-record-audio @result="onResult" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
+  },
+  methods: {
+    onResult (data) {
+      console.log('The blob data:', data);
+      console.log('Downloadable audio', window.URL.createObjectURL(data));
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
